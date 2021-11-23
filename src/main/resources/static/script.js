61a93816a88b21d9a47c3b465b77da4f298f3051
Gerónimo <geroortizz123@gmail.com>
2021-11-23 17:29:36 -0300

    error fix

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,7 +1,7 @@
 var addTask = (event) => {
     var action = event.target.getAttribute("href");
     document.querySelector("#container").innerHTML =
-        "<h2 class=\"d-none\" id=\"title-edit\"> Editing Task</h2>" +
+        "<h2 class=\"\" id=\"title-edit\"> Editing Task</h2>" +
         "<form th:replace=\"components/editForm :: _editForm\"></form>";
     document.getElementById("form-edit").action = action;
     // document.getElementById("title").classList.add("d-none");
