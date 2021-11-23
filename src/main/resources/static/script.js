84fdaf13e8ff06d129a2740f6c1eaf189fb1f2ba
Gerónimo <geroortizz123@gmail.com>
2021-11-23 17:25:38 -0300

    cambios

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,11 +1,14 @@
 var addTask = (event) => {
     var action = event.target.getAttribute("href");
+    document.querySelector("#container").innerHTML =
+        "<h2 class=\"d-none\" id=\"title-edit\"> Editing Task</h2>" +
+        "<form th:replace=\"components/editForm :: _editForm\"></form>";
     document.getElementById("form-edit").action = action;
-    document.getElementById("title").classList.add("d-none");
-    document.getElementById("title-edit").classList.remove("d-none");
-    document.getElementById("list").classList.add("d-none");
-    document.getElementById("form-task").classList.add("d-none");
-    document.getElementById("form-edit").classList.remove("d-none");
+    // document.getElementById("title").classList.add("d-none");
+    // document.getElementById("title-edit").classList.remove("d-none");
+    // document.getElementById("list").classList.add("d-none");
+    // document.getElementById("form-task").classList.add("d-none");
+    // document.getElementById("form-edit").classList.remove("d-none");
 };
 
 var changeMarked = (event) => {
