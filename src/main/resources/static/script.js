05c23c863ec7f1feb5f76c25ffa9b59783a80492
Gerónimo <geroortizz123@gmail.com>
2021-11-23 17:35:21 -0300

    cambio

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,7 +2,16 @@ var addTask = (event) => {
     var action = event.target.getAttribute("href");
     document.querySelector("#container").innerHTML =
         "<h2 class=\"\" id=\"title-edit\"> Editing Task</h2>" +
-        "<form th:replace=\"components/editForm :: _editForm\"></form>";
+        "<form th:fragment=\"_editForm\" th:action=\"@{/editTask/{id}(id=${task.id})}\" th:object=\"${task}\" method=\"post\" class=\"w-50 p-3 mx-auto \"\n" +
+        "      id=\"form-edit\">\n" +
+        "    <div class=\"form-group\">\n" +
+        "        <input type=\"text\" placeholder=\"Task...\" class=\"w-75\" th:field=\"*{title}\" th:text=\"${task.title}\">\n" +
+        "    </div>\n" +
+        "    <div class=\"d-flex mx-auto justify-content-around w-50\">\n" +
+        "        <button type=\"submit\" class=\"btn btn-outline-secondary\">Save</button>\n" +
+        "        <a href=\"\" class=\"btn btn-outline-secondary\">Cancel</a>\n" +
+        "    </div>\n" +
+        "</form>";
     document.getElementById("form-edit").action = action;
     // document.getElementById("title").classList.add("d-none");
     // document.getElementById("title-edit").classList.remove("d-none");
